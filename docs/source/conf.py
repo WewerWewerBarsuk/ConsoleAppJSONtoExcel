102b2a9935b12f0108bc47fef322debc5c02ab41
Oleg Barsukov <114997265+WewerWewerBarsuk@users.noreply.github.com>
2024-05-24 12:23:26 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ConsoleAppJSONtoExcel'
+copyright = '2024, Oleg Barsukov'
+author = 'Oleg Barsukov'
 
 release = '0.1'
 version = '0.1.0'
